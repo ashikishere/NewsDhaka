9d333e78c80024d108d7e6a8ec09c3f3915b38d2
ashikishere <azashikishere@gmaol.com>
2021-12-31 03:14:00 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NewsToday</title>
+    <style>
+    body {
+        font-family: monospace;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        background-color: #ECEBEC;
+    }
+
+    h1 {
+        font-size: 40px;
+        margin-bottom: 0px;
+
+    }
+
+    h4 {
+        font-size: 20px;
+        color: gray;
+    }
+    </style>
+</head>
+
+<body>
+    <h1>Website Under Construction</h1>
+    <h4>something is comming better!!</h4>
+</body>
+
+</html>
