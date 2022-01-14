159f2c61e6d50049c241122ee3be79f264b5fc6c
ashikishere <azashikishere@gmaol.com>
2022-01-14 13:27:52 +0600

    all post added

--- inc/header.php
+++ inc/header.php
@@ -12,7 +12,9 @@ include "admin/inc/connection.php";
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+
     <title>NewsToday | home</title>
 </head>
 
@@ -29,9 +31,24 @@ include "admin/inc/connection.php";
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
+                        <?php
+
+                        $sql = "SELECT * FROM category WHERE c_status=1";
+                        $navber_item = mysqli_query($db, $sql);
+                        while ($row = mysqli_fetch_assoc($navber_item)) {
+                            $c_id  = $row['c_id'];
+                            $c_name = $row['c_name'];
+
+                        ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="post.php?category=<?php $c_id; ?>"><?php echo $c_name; ?></a>
                         </li>
+                        <?php
+                        }
+
+                        ?>
+
+
                     </ul>
                 </div>
             </div>
--- index.php
+++ index.php
@@ -6,7 +6,60 @@ include "inc/header.php";
     <div class="container">
         <div class="row">
             <div class="col-md-8">
-                <h1>All Content Here</h1>
+                <h1>All Post Here</h1>
+
+                <div class="all-post">
+                    <?php
+
+                    $sql = "SELECT * FROM post ORDER BY post_id DESC";
+                    $all_post = mysqli_query($db, $sql);
+
+                    while ($row = mysqli_fetch_assoc($all_post)) {
+                        $post_id = $row['post_id'];
+                        $title = $row['title'];
+                        $thumbnail = $row['thumbnail'];
+                        $description = $row['description'];
+                        $category_id = $row['category_id'];
+                        $postDate = $row['postDate'];
+                        $first25words = implode(' ', array_slice(str_word_count($description, 1), 0, 25)); ?>
+                    <div class="single-post">
+                        <img src="admin/assets/images/post/<?php echo $thumbnail; ?>"
+                            alt="" class="post-img">
+                        <a href="single.php?post=<?php echo $post_id; ?>"
+                            class="title-name">
+                            <h3><?php echo $title; ?>
+                            </h3>
+                        </a>
+                        <p><?php echo $first25words; ?>
+                            ........
+                            <br>
+                            <a href="single.php?post=<?php echo $post_id; ?>"
+                                class="btn btn-primary">READ MORE</a>
+                        </p>
+                        <p>category- <span class="badge bg-success"><?php
+                        $sql = "SELECT * FROM category WHERE c_id ='$category_id'";
+                        $all_cat = mysqli_query($db, $sql);
+                        while ($row = mysqli_fetch_assoc($all_cat)) {
+                            $c_id = $row['c_id'];
+                            $c_name = $row['c_name'];
+                        }
+
+                        echo $c_name; ?>
+                            </span> || posted by - Ashik ||
+
+
+                            <span>Publish
+
+                                Date:<?php echo $postDate; ?>
+                            </span>
+                        </p>
+
+                    </div>
+
+                    <?php
+                    }
+                    ?>
+                </div>
             </div>
             <div class="col-md-4">
                 <?php
@@ -20,4 +73,3 @@ include "inc/header.php";
 
 <?php
 include "inc/footer.php";
-?>
